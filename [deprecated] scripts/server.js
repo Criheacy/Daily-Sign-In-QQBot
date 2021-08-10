f1d7ac145cf0d28c4125851732a6b176dd4eb287
Criheacy <hekaiqi@mail.sdu.edu.cn>
2021-08-10 23:06:47 +0800

    feat: Add message listener

--- [deprecated] scripts/server.js
+++ [deprecated] scripts/server.js
@@ -1,2 +1,23 @@
 const express = require('express');
+const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.post('/', function (req, res) {
+  const data = req.body;
+  if (data.post_type && data.post_type === "message") {
+    console.log(`Receive [${data.message}] from ${data.sender.nickname}`);
+    console.log(data);
+  }
+  res.status(204).end();
+})
+
+const server = app.listen(9000, function () {
+	const host = server.address().address
+	const port = server.address().port
+
+	console.log(`Listening at http://${host}:${port}`);
+})
